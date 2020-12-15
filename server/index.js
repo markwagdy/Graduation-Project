50706cb7ec6c8541a68ebd00c217d23bad0508b6
Mark Wagdy <16P8146@eng.asu.edu.eg>
2020-12-15 14:05:26 +0200

    course router added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const db = require('./data/db')
 const studentRouter=require('./routes/student-router')
+const courseRouter=require('./routes/course-router')
 
 const app = express()
 const apiPort = 3000
@@ -17,5 +18,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 app.use('/api',studentRouter)
+app.use('/api',courseRouter)
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))

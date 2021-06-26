808bbedd76c192334a4d08357132eb1145292a24
Mark Wagdy <16P8146@eng.asu.edu.eg>
2021-06-26 19:16:24 +0200

    Call Grabs frame and send it to the server flask Succesfully

--- server/public/script.js
+++ server/public/script.js
@@ -1,31 +1,72 @@
+
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined)
 const myVideo = document.createElement('video')
 myVideo.muted = true
-
-let canvas=document.querySelector('#canvas');
-let contect=canvas.getContext('2d');
+var img=new Image()
+let canvas=document.querySelector('#canvas')
+let context=canvas.getContext("2d");
+var dataUrl=canvas.toDataURL("image/png");
+var xht=new XMLHttpRequest()
+let form=new FormData();
 
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
   addVideoStream(myVideo, stream)
-
+  
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
+    video.id='myvideo'
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })  })
     socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
-
 })
+var azhryCounter=0;
+function AzhryZoz(){
+  azhryCounter++;
+
+  console.log("Azhry Counter"+azhryCounter)
+  img.onload=()=>{
+    context.drawImage(myVideo,0,0,540,380)
+    console.log(canvas.toDataURL())
+    $.ajax({
+      url:'http://localhost:5000/maskImage',
+      type:'POST',
+      data:canvas.toDataURL(),
+      cache:false,
+      processData:false,
+      contentType:false,
+      error: function(data){
+            console.log("upload error" , data);
+            console.log(data.getAllResponseHeaders());
+          },
+      success: function(data){
+          console.log(data['status'])
+  }
+    })
+  };
+  img.src=dataUrl;
+  
+  
+  
+  console.log(dataUrl)
+}
+
+setInterval(
+
+ 
+AzhryZoz
 
 
+,60000);
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
@@ -48,7 +89,6 @@ function connectToNewUser(userId, stream) {
   
 }
 
-
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
@@ -56,5 +96,4 @@ function addVideoStream(video, stream) {
   })
   videoGrid.append(video)
 }
-
 
